40999ab5e823dbaa443c3a1249867ce796144da1
ThibaultBarral <thibault.barral3347@gmail.com>
2021-09-08 09:28:13 +0200

    Updating home page

--- resources/views/home/section-3.blade.php
+++ resources/views/home/section-3.blade.php
@@ -1,9 +1,16 @@
 <section id="part-3">
     <div class="flex">
         <div class="box">
+            <div>
+                <h2>Jouer</h2>
+                <p>Vous pouvez dès à présent profiter d'une partie de babyfoot entre amis. Vous retrouverez tous les résultats sur votre profil.</p>
+            </div>  
+        <div class="img"></div>
+        <div class="intro">
             <h2>Jouer</h2>
             <p>Vous pouvez dès à présent profiter d'une partie de babyfoot entre amis. Vous retrouverez tous les résultats sur votre profil.</p>
-        <div class="img"></div>
         </div>
+        </div>
+    </div>
     </div>
 </section>
