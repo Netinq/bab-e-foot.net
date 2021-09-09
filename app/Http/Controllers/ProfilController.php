a434888fdb7c68f4a9662118acf72f29c5a28f1a
BuildTools <sarquentin@gmail.com>
2021-09-10 01:54:51 +0200

    Adding level system

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
+use App\Models\Goal;
 use App\Models\Stat;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -21,6 +23,19 @@ class ProfilController extends Controller
         $user = User::where('id', $uuid)->first();
         $stat = Stat::where('player_id', $uuid)->first();
         $qr = QrCode::size(500)->generate(route('qr', $uuid));
-        return view('profil.index', compact('qr', 'user', 'stat'));
+
+        $games = Game::where('player_1', $uuid)->orWhere('player_2', $uuid)->get();
+        foreach ($games as $game) {
+            $player_1 = $game->player_1;
+            $player_2 = $game->player_2;
+            $game->goals = Goal::where('game_id', $game->id)->where('player_id', $uuid)->count();
+            if ($player_1 == $uuid) $enemy_goals = Goal::where('game_id', $game->id)->where('player_id', $player_2)->count();
+            else $enemy_goals = Goal::where('game_id', $game->id)->where('player_id', $player_1)->count();
+            if ($player_1 == $uuid) $game->enemy_name = User::where('id', $player_2)->first()->username;
+            else $enemy_goals = $game->enemy_name = User::where('id', $player_1)->first()->username;
+            $game->win = $game->goals > $enemy_goals ? true : false;
+        }
+
+        return view('profil.index', compact('qr', 'user', 'stat', 'games'));
     }
 }
--- resources/views/profil/index.blade.php
+++ resources/views/profil/index.blade.php
@@ -11,16 +11,21 @@
         <p>{{ $user->username }}</p>
         <a href="" class="modifier"><img src="{{ asset('svg/pen.svg')}}" alt="Modifier"></a>
     </div>
-    <a href="{{route('logout')}}" class="exit"><img src="{{ asset('svg/exit.svg')}}" alt="Exit">
-        <p>Déconnexion</p>
-    </a>
+    <form action="{{route('logout')}}" method="post">
+        @csrf
+        <button type="submit" class="exit"><img src="{{ asset('svg/exit.svg')}}" alt="Exit">
+            <p>Déconnexion</p>
+        </button>
+    </form>
     <div class="level">
         <div>
             <img src="{{ asset('svg/stats.svg')}}" alt="Stats">
             <h2>Mes Statistiques</h2>
         </div>
-        <p>Niveau 0</p>
-        <div class="skillbar"></div>
+        <p id="level">Niveau 0</p>
+        <div class="skillbar">
+            <div id="skill"></div>
+        </div>
     </div>
     <div class="stats">
         <div class="stats-1">
@@ -45,24 +50,36 @@
             <img src="{{ asset('svg/history.svg')}}" alt="History">
             <h2>Historique</h2>
         </div>
-        <diV class="results-history-v">
+        @foreach ($games as $game )
+        <diV class="{{ $game->win ? 'results-history-v' : 'results-history-d'}}">
             <div>
-                <h2>Victoire</h2>
-                <p>Vous avez affronté <span>Quentin</span></p>
+                <h2>{{ $game->win ? 'Victoire' : 'Défaite'}}</h2>
+                <p>Vous avez affronté <span>{{$game->enemy_name}}</span></p>
             </div>
-            <div class="circle-v">
-                <p><span>01</span>buts</p>
+            <div class="{{ $game->win ? 'circle-v' : 'circle-d'}}">
+                <p><span>{{$game->goals}}</span>buts</p>
             </div>
         </diV>
-        <diV class="results-history-d">
-            <div>
-                <h2>Défaite</h2>
-                <p>Vous avez affronté <span>Jules</span></p>
-            </div>
-            <div class="circle-d">
-                <p><span>01</span>buts</p>
-            </div>
-        </diV>
+        @endforeach
     </div>
 </section>
+
+<script>
+    window.addEventListener('load', () => {
+
+        const skill = document.getElementById('skill');
+        const points = {!! json_encode($stat->points) !!};
+        let level = 1;
+        let exp = 9;
+        for (let e = 9; 1.5**e < points; e++)
+        {
+            level++;
+            exp += 1;
+        }
+        let percentage = (points - 1.5**(exp-1)) * 100 / (1.5**(exp)-1.5**(exp-1));
+        skill.style.width = percentage+'%';
+        document.getElementById('level').textContent = 'Niveau '+level;
+    });
+</script>
+
 @endsection
