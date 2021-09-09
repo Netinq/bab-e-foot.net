eeaf4ffb5f7f541089ea3aa88216cc2a391e352b
ThibaultBarral <thibault.barral3347@gmail.com>
2021-09-09 11:33:55 +0200

    Fixing homepage & Adding profil page

--- resources/views/profil/index.blade.php
+++ resources/views/profil/index.blade.php
@@ -4,5 +4,37 @@
 ])
 
 @section('content')
-{!! $qr !!}
+<section id="part-profil">
+    <img src="{{ asset('img/logo-txt.png')}}" alt="Logo Bab-E-Foot">
+    <div class="qrcode">
+        {!! $qr !!}
+        <p>thibault.barral</p>
+        <a href="" class="modifier"><img src="{{ asset('svg/pen.svg')}}" alt="Modifier"></a>
+    </div>
+    <a href="" class="exit"><img src="{{ asset('svg/exit.svg')}}" alt="Exit">
+        <p>Déconnexion</p>
+    </a>
+    <div class="level">
+        <div>
+            <img src="{{ asset('svg/stats.svg')}}" alt="Stats">
+            <h2>Mes Statistiques</h2>
+        </div>
+        <p>Niveau 25</p>    
+        <div class="skillbar"></div>
+    </div>
+    <div class="stats">
+        <div class="stats-1">
+            
+        </div>
+        <div class="stats-2">
+            
+        </div>
+        <div class="stats-3">
+            
+        </div>
+        <div class="stats-4">
+            
+        </div>
+    </div>
+</section>
 @endsection
